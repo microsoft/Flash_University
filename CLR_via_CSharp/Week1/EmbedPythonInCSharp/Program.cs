5c3139f61cb4cf8b617fee744a4cbf9498568bc4
rakirs2 <srmylavarapu@microsoft.com>
2022-07-06 14:47:50 -0500

    didn't have it set up before

---
+++ CLR_via_CSharp/Week1/EmbedPythonInCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//This was blindly taken from https://github.com/pythonnet/pythonnet/blob/master/README.rst
+
+static void Main(string[] args)
+{
+    PythonEngine.Initialize();
+    using (Py.GIL())
+    {
+        dynamic np = Py.Import("numpy");
+        Console.WriteLine(np.cos(np.pi * 2));
+
+        dynamic sin = np.sin;
+        Console.WriteLine(sin(5));
+
+        double c = (double)(np.cos(5) + sin(5));
+        Console.WriteLine(c);
+
+        dynamic a = np.array(new List<float> { 1, 2, 3 });
+        Console.WriteLine(a.dtype);
+
+        dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
+        Console.WriteLine(b.dtype);
+
+        Console.WriteLine(a * b);
+        Console.ReadKey();
+    }
+}
---
+++ CLR_via_CSharp/Week1/HelloWorld/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("If you can read this, there now a positive confidence interval that things will work");
