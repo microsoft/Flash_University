fedb57b8d85212b382f1712507114d756dfcabb4
rakirs2 <srmylavarapu@microsoft.com>
2022-07-13 08:47:40 -0500

    adding object functions

---
+++ CLR_via_CSharp/Week2/ObjectFunctions/Program.cs
@@ -0,0 +1,30 @@
+﻿//this is a namespace --> essentially it's shorthand for us
+using System.Runtime.CompilerServices;
+
+Object O = new Object();
+Object R = new Object();
+
+//publicly available methods
+Console.WriteLine(O.Equals(O));
+Console.WriteLine(O.Equals(R));
+
+Horse horse = new Horse();
+Horse horse2 = new Horse();
+
+Console.WriteLine(horse.GetType());
+
+
+
+Console.WriteLine(horse.ToString());
+Console.WriteLine(horse.GetHashCode());
+
+//Guess the output, this is discussed more in the next chapter
+//Console.Write(horse2.Equals(horse));
+class Horse
+{
+    /*public override string ToString()
+    {
+        return "not 'Horse'";
+    }*/
+
+}
