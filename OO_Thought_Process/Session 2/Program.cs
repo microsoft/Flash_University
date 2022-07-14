ad3cc6b0adf0e81b8d3b06e575cd4552377252d3
Tate Burns <tateburns@microsoft.com>
2022-07-13 20:22:48 -0500

    add oo session 2 example

---
+++ OO_Thought_Process/Session 2/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+ * The example for session 2 is about interface design, and is not runnable.
+ * 
+ * Open the Taxi/Taxi.cs file to view the example.
+ */
+
+Console.WriteLine("The session 2 example is not runnable");
