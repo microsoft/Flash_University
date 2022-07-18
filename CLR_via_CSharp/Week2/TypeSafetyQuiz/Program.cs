7a1da5a688837d5debb8f8637b7126095b22c47d
rakirs2 <srmylavarapu@microsoft.com>
2022-07-18 11:18:35 -0500

    updating type safety quiz

---
+++ CLR_via_CSharp/Week2/TypeSafetyQuiz/Program.cs
@@ -0,0 +1,25 @@
+﻿using TypeSafetyQuiz;
+
+Console.WriteLine("This is basically taken exactly from  pg. 97 w/ a few 'better' naming conventions");
+Console.WriteLine("The whole program is commented out as compile time exceptions will be caught w/ VS. Try guessing where exceptions could occur and some fixes around them");
+//
+// Object object1 = new Object();
+// Object object2 = new BaseClass();
+// Object object3 = new DerivedClass();
+// Object object4 = object3;
+//
+// BaseClass base1 = new BaseClass();
+// BaseClass base2 = new DerivedClass();
+// DerivedClass derived1 = new DerivedClass();
+//
+// BaseClass base3 = new Object();
+// DerivedClass derived2 = new Object();
+// BaseClass base4 = derived1;
+// DerivedClass derived3 = base2;
+// DerivedClass derived4 = (DerivedClass)derived1;
+// DerivedClass derived5 = (DerivedClass) base2;
+// DerivedClass derived6 = (DerivedClass) base1;
+// BaseClass base5 = (BaseClass) object1;
+// BaseClass base6 = (DerivedClass) object2;
+
+// Bonus, add some checking to be able to run the program to avoid the RTEs (is/as/something else)
