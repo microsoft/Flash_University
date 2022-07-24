eea3bbd7eff666df0dc3f8e16814c0b8799b8a4e
rakirs2 <srmylavarapu@microsoft.com>
2022-07-24 13:25:20 -0500

    adding week 3

---
+++ CLR_via_CSharp/Week3/PrimitiveTypes/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Primitive Types map their values directly to FCL (Framework class libraries) types");
+Console.WriteLine("In effect, this means that ");
