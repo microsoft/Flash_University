6add548a737270b3c53295984d271e7146fe3e90
Tate Burns <tateburns@microsoft.com>
2022-08-10 10:41:23 -0500

    Add Command pattern example

---
+++ Design_Patterns/Design Patterns/Command/CommandRunner.cs
@@ -0,0 +1,73 @@
+﻿namespace Command;
+
+/**
+ * The Command runner
+ *
+ * This is the "Invoker" object in the Command pattern
+ *
+ * It reads input from the command line and executes the corresponding Command.
+ * It works with any commands, since it doesn't need to know what they do.
+ */
+public class CommandRunner
+{
+    private readonly Dictionary<string, Command> _commandTable;
+
+    public CommandRunner(Dictionary<string, Command> commandTable)
+    {
+        _commandTable = commandTable;
+        _commandTable.Add("help", new Help(this._commandTable));
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("(type 'help' for help, and 'quit' or 'exit' to exit)");
+
+        var commandName = Console.ReadLine();
+        while (commandName != null && commandName != "quit" && commandName != "exit")
+        {
+            if (_commandTable.ContainsKey(commandName))
+            {
+                _commandTable[commandName].Execute();
+            }
+            else
+            {
+                Console.WriteLine("unknown command: {0}", commandName);
+            }
+
+            commandName = Console.ReadLine();
+        }
+    }
+}
+
+/**
+ * The Help command
+ *
+ * When invoked, prints out all of the available commands (key, name, and description)
+ *
+ * This command doesn't contain a receiver; it directly invokes Console methods (since they are static).
+ * I could have added a Console facade object and used that as the receiver.
+ */
+public class Help : Command
+{
+    private readonly Dictionary<string, Command> _commands;
+
+    public Help(Dictionary<string, Command> commands)
+    {
+        _commands = commands;
+    }
+
+    public override string Name => "Help";
+    public override string Description => "displays the help message";
+
+    public override void Execute()
+    {
+        Console.WriteLine("\nHere are the available commands:");
+        
+        foreach (var commandEntry in _commands)
+        {
+            Console.WriteLine("{0} - {1} ({2})", commandEntry.Key, commandEntry.Value.Name, commandEntry.Value.Description);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ Design_Patterns/Design Patterns/Command/CounterCli.cs
@@ -0,0 +1,44 @@
+﻿namespace Command;
+
+/**
+ * This is the main class that sets everything up and runs it.
+ *
+ * It instantiates a counter (the receiver), it instantiates
+ * all the counter commands (with the counter object),
+ * passes them to the invoker, and runs the invoker.
+ */
+public class CounterCli
+{
+    public void Run()
+    {
+        var counter = new Counter();
+
+        var commandTable = new Dictionary<string, Command>
+        {
+            { "inc", new Increment(counter) },
+            { "dec", new Decrement(counter) },
+            { "reset", new SetToZero(counter) },
+            { "print", new PrintValue(counter) },
+        };
+
+        Console.WriteLine("Welcome to the Counter CounterCli");
+
+        var runner = new CommandRunner(commandTable);
+        runner.Run();
+
+        Console.WriteLine("\nfinal counter value: {0}", counter.Value);
+    }
+}
+
+public class PrintValue : CounterCommand
+{
+    public PrintValue(Counter counter) : base(counter) { }
+
+    public override string Name => "PrintValue";
+    public override string Description => "prints the value of the counter";
+
+    public override void Execute()
+    {
+        Console.WriteLine("counter: {0}", Counter.Value);
+    }
+}
