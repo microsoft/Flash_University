543b59d91128add50f4269439caf9b3e0bc61065
Tate Burns <tateburns@microsoft.com>
2022-08-12 16:36:12 -0500

    added oo session 6 examples

---
+++ OO_Thought_Process/Session 6/Examples/NonReusable/TestShop.cs
@@ -0,0 +1,45 @@
+﻿namespace Session_6.Examples.NonReusable;
+
+internal class TestNonReusableShop
+{
+    public static void Run()
+    {
+        RunPizzaShop();
+        Console.WriteLine();
+        RunDonutShop();
+    }
+
+    public static void RunPizzaShop()
+    {
+        Console.WriteLine("Creating the Pizza shop");
+        var pizzaShop = new PizzaShop();
+
+        // list the inventory
+        var inventory = pizzaShop.GetInventory();
+
+        for (var i = 0; i < inventory.Length; i++)
+        {
+            Console.WriteLine("Item " + i + " = " + inventory[i]);
+        }
+
+        // buy an item
+        pizzaShop.BuyInventory(inventory[1]);
+    }
+
+    public static void RunDonutShop()
+    {
+        Console.WriteLine("Creating the Donut shop");
+        var donutShop = new DonutShop();
+
+        // list the inventory
+        var inventory = donutShop.GetInventory();
+
+        for (var i = 0; i < inventory.Length; i++)
+        {
+            Console.WriteLine("Item " + i + " = " + inventory[i]);
+        }
+
+        // buy an item
+        donutShop.BuyInventory(inventory[1]);
+    }
+}
---
+++ OO_Thought_Process/Session 6/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Session_6.Examples.NonReusable;
+using Session_6.Examples.Reusable;
+
+Console.WriteLine(" --- Running the non-reusable shop tests ---\n");
+
+TestNonReusableShop.Run();
+Console.WriteLine();
+
+Console.WriteLine(" --- Running the non-reusable shop tests ---\n");
+
+
+TestReusableShop.Run(typeof(ReusablePizzaShop));
+Console.WriteLine();
+
+TestReusableShop.Run(typeof(ReusableDonutShop));
