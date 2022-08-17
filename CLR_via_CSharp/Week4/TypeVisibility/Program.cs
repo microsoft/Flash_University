388b84eb48460f38ede2b9ca90c4f466c330cda5
rakirs2 <srmylavarapu@microsoft.com>
2022-08-17 11:21:33 -0500

    adding type visibility/friend visibility

---
+++ CLR_via_CSharp/Week4/TypeVisibility/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("ForProgrammingPurposes");
+public class MeaningOfLife{
+    public MeaningOfLife() { }
+
+}
+
+internal class Area51
+{
+    internal Area51() { }
+
+}
