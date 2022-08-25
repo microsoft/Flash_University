bf3680869f55486af65aa374485780ccd905ded5
rakirs2 <srmylavarapu@microsoft.com>
2022-08-24 19:02:07 -0500

    adding base code fro why certain modifiers don't work

---
+++ CLR_via_CSharp/Chapter8/ConstructorsForReferenceTypes/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConstructorsForReferenceTypes;
+
+Console.WriteLine("Hello, World!");
+ReferenceClass reference = new ReferenceClass();
---
+++ CLR_via_CSharp/Chapter8/ConstructorsForReferenceTypes/ReferenceClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsForReferenceTypes
+{
+    internal class ReferenceClass
+    {
+        ////these are   not valid because constructors can't be inherited
+        //virtual ReferenceClass()
+        //{
+        //    //virtual --> can be overridden later
+        //}
+
+        //new ReferenceClass()
+        //{
+        //    //new --> redefine method in derived class
+        //}
+
+        //override ReferenceClass()
+        //{
+        //    // new implemention from a base class
+        //}
+
+        //sealed ReferenceClass()
+        //{
+        //    // class cannot be derived form
+        //}
+
+        //abstract ReferenceClass()
+        //{
+        //    //will be overridden in a derived class
+        //}
+    }
+}
