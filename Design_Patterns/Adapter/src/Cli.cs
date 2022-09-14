8c8be33a58f8a5fedc60f9dc1b3f2e329018fa28
Tate Burns <tateburns@microsoft.com>
2022-09-14 15:11:15 -0500

    add the Adapter pattern

---
+++ Design_Patterns/Adapter/src/Cli.cs
@@ -0,0 +1,146 @@
+﻿namespace Adapter;
+
+/**
+ * Simple interface used for transforming text
+ *
+ * This is the "Target" interface
+ */
+public interface ITransformer
+{
+    public void SetText(string text);
+    public string GetBaseText();
+
+    public string Uppercase();
+    public string Exclaim();
+    public string Shout();
+    public string Whisper();
+}
+
+/**
+ * A simple CLI for transforming text
+ *
+ * This is the "Client"
+ *
+ * The CLI uses a ITransformer instance to do the text
+ * transformation - it doesn't care what actually implements
+ * the functionality, it just has to match the ITransformer interface.
+ *
+ * (All the command methods are in the "commands" region)
+ */
+public class Cli
+{
+    private readonly ITransformer _transformer;
+
+    /*
+     * The ITransformer object is passed in through this constructor,
+     * hence the CLI has no knowledge (nor does it care) what
+     * actual class it is
+     */
+    public Cli(ITransformer transformer)
+    {
+        _transformer = transformer;
+    }
+
+    #region command runner
+
+    public void Run()
+    {
+        Console.WriteLine("Text Transforming System");
+
+        var command = GetNextCommand();
+
+        while (command != null && command != "quit" && command != "exit")
+        {
+            RunCommand(command);
+            command = GetNextCommand();
+        }
+    }
+
+    private string? GetNextCommand()
+    {
+        Console.Write("\n> ");
+        return Console.ReadLine();
+    }
+
+    private void RunCommand(string command)
+    {
+        try
+        {
+            if (command == "set-text")
+            {
+                SetText();
+            }
+            else if (command == "show")
+            {
+                Show();
+            }
+            else if (command == "uppercase")
+            {
+                Uppercase();
+            }
+            else if (command == "exclaim")
+            {
+                Exclaim();
+            }
+            else if (command == "shout")
+            {
+                Shout();
+            }
+            else if (command == "whisper")
+            {
+                Whisper();
+            }
+            else
+            {
+                Console.WriteLine("unknown command: {0}", command);
+                Console.WriteLine("valid commands are: " +
+                                  "'set-text', 'show', 'uppercase', 'exclaim', 'shout', 'whisper', 'quit', 'exit'");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e.Message);
+        }
+    }
+
+    #endregion
+
+    #region commands
+
+    public void SetText()
+    {
+        Console.Write("text: ");
+        var text = Console.ReadLine() ?? "";
+
+        _transformer.SetText(text);
+
+        Console.WriteLine("updated base text");
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(_transformer.GetBaseText());
+    }
+
+    public void Uppercase()
+    {
+        Console.WriteLine(_transformer.Uppercase());
+    }
+
+    public void Exclaim()
+    {
+        Console.WriteLine(_transformer.Exclaim());
+    }
+
+    public void Shout()
+    {
+        Console.WriteLine(_transformer.Shout());
+    }
+
+    public void Whisper()
+    {
+        Console.WriteLine(_transformer.Whisper());
+    }
+
+    #endregion
+}
---
+++ Design_Patterns/Adapter/src/TextManipulator.cs
@@ -0,0 +1,105 @@
+﻿namespace Adapter;
+
+/**
+ * A Utility for manipulating text in various ways
+ *
+ * This is the "Adaptee"
+ *
+ * The interface of this class clearly does not match
+ * the ITransformer that the CLI needs. In addition,
+ * this class is missing a slight piece of functionality -
+ * you can change the source text, it's only set in
+ * the constructor.
+ *
+ * The Transformer class will implement the ITransformer
+ * interface in front of this class, and it will also
+ * implement the missing behavior (updating the source
+ * text on the fly)
+ *
+ * NOTE - in this example it is easy to update this class
+ * to match the ITransformer interface; however, in real
+ * cases this class wouldn't be able to be updated for various
+ * reasons (it's 3rd-party code, the existing interface is
+ * being used somewhere else, etc), and thus the "adapter"
+ * is necessary.
+ */
+public class TextManipulator
+{
+    public string Source { get; }
+    private readonly Random _random = new();
+
+    /*
+     * As mentioned above, the source text is set
+     * at construction time and cannot be updated
+     */
+    public TextManipulator(string source)
+    {
+        Source = source;
+    }
+
+    /**
+     * Manipulate the source string based on the passed options
+     *
+     * This is the only method exposed - it contains all of the
+     * functionality that the CLI needs, but it doesn't match the
+     * ITransformer interface.
+     */
+    public string Manipulate(Options options)
+    {
+        var text = Source;
+
+        if (options.Prefix != null) text = options.Prefix + text;
+
+        if (options.Postfix != null) text = text + options.Postfix;
+
+        if (options.Interspersed != null) text = Intersperse(text, options.Interspersed);
+
+        if (options.Case == TextCase.Uppercase)
+            text = text.ToUpper();
+        else if (options.Case == TextCase.Lowercase) text = text.ToLower();
+
+        return text;
+    }
+
+    private string Intersperse(string text, string interspersion)
+    {
+        var words = new List<string>(text.Split(" "));
+        var textWithInterspersion = "";
+
+        while (words.Count > 0)
+        {
+            var numWords = _random.Next(1, 4);
+
+            if (numWords >= words.Count)
+            {
+                textWithInterspersion += string.Join(" ", words);
+                words.Clear();
+            }
+            else
+            {
+                textWithInterspersion += string.Join(" ", words.Take(numWords)) + $" {interspersion} ";
+                words.RemoveRange(0, numWords);
+            }
+        }
+
+        return textWithInterspersion;
+    }
+
+    #region helper classes
+
+    public enum TextCase
+    {
+        Uppercase,
+        Lowercase
+    }
+
+    public class Options
+    {
+        public TextCase? Case { get; set; }
+        public string? Prefix { get; set; }
+        public string? Postfix { get; set; }
+        public string? Interspersed { get; set; }
+    }
+
+    #endregion
+}
