d29666705dc9532fc98ec975006f6fec73d17dc4
rakirs2 <srmylavarapu@microsoft.com>
2022-10-04 12:45:02 -0500

    updating files

--- CLR_via_CSharp/Chapter 1-3 - Introduction/HelloWorld/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("If you can read this, there now a positive confidence interval that things will work");
-//To view IL of this code, or any c# code, use ildasm on the pe file
