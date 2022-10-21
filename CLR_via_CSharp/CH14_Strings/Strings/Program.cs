03e64199c77654b36c29d41a7ad05942f1c9331c
rakirs2 <srmylavarapu@microsoft.com>
2022-10-20 20:13:34 -0500

    adding strings dir

---
+++ CLR_via_CSharp/CH14_Strings/Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿// This program is just showing the basics
+
+var min = char.MinValue;
+var max = char.MaxValue;
+
+Console.WriteLine(min);
+Console.WriteLine(max);
+
+int i_a = min;
+int i_b = max;
+
+Console.WriteLine(i_a);
+
+Console.WriteLine(i_b);
+
+var a = 'a';
+Console.WriteLine(a);
+Console.WriteLine((int)a);
---
+++ CLR_via_CSharp/CH14_Strings/Strings/Strings.md
@@ -0,0 +1,13 @@
+# Strings
+
+## Questions from last time 
+- none I can remember
+
+## Things to look for today
+- representations of a string
+    - String min/max
+- transformations of a string
+- hexadecimal basics
+- what is a culture
+- how are strings represented in the CLR
+- Casting/converstions
