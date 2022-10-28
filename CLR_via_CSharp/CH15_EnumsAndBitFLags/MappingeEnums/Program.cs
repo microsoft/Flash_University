7d7d830856c0ad15224daebce98a174d19350208
rakirs <srikarmylavarapu@gmail.com>
2022-10-27 20:50:34 -0500

    adding extra files

---
+++ CLR_via_CSharp/CH15_EnumsAndBitFLags/MappingeEnums/Program.cs
@@ -0,0 +1,13 @@
+﻿Color c = Color.Red;
+Console.WriteLine(c);
+Console.WriteLine(c.ToString());
+
+//this is some formatting fun -- things to keep from last time, general, decimal,hex
+
+Console.WriteLine(c.ToString("G"));
+Console.WriteLine(c.ToString("D"));
+Console.WriteLine(c.ToString("X"));
+enum Color: byte
+{
+    Red,Orange, Yellow,Green,Blue,Indigo,Violet
+}
