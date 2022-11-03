0f87a481646b54651c3ea2c1eb2a02d9367074e9
rakirs <srikarmylavarapu@gmail.com>
2022-11-03 18:32:31 -0500

    updating

---
+++ CLR_via_CSharp/CH16_Arrays/ValueReference/ValueReference.cs
@@ -0,0 +1,39 @@
+﻿int[] fibonacci;
+fibonacci = new int[10];
+
+foreach (int i in fibonacci)
+{
+    Console.WriteLine(i);
+}
+
+var references = new UsefulClass[2];
+
+//What do we expect here. 
+
+// foreach (var i in references)
+// {
+//     Console.WriteLine(i.X);
+// }
+
+
+for (int i = 0; i < references.Length; i++)
+{
+    references[i] = new UsefulClass();
+}
+
+foreach (var i in references)
+{
+    Console.WriteLine(i.X);
+}
+
+
+internal class UsefulClass
+{
+    public int X { get; set; } 
+
+    public UsefulClass()
+    {
+        X = 0;
+    }
+}
+
