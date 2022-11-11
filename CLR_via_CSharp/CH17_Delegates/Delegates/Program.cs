320047a8969f04c6245147e8ed02c5780dde847e
rakirs <srikarmylavarapu@gmail.com>
2022-11-10 19:33:51 -0600

    adding program

--- CLR_via_CSharp/CH17_Delegates/Delegates/Program.cs
+++ CLR_via_CSharp/CH17_Delegates/Delegates/Program.cs
@@ -6,21 +6,21 @@ internal sealed class DelegateIntro {
    public static void Main() {
       StaticDelegateDemo();
    }
-
+   
    private static void StaticDelegateDemo() {
       Console.WriteLine("----- Static Delegate Demo -----");
       Counter(1, 3, null);
       Counter(1, 3, new Feedback(DelegateIntro.FeedbackToConsole));
       Console.WriteLine();
    }
-
+   
    private static void SimplifiedDelegateDemo() {
       Console.WriteLine("----- Static Delegate Demo -----");
       Counter(1, 3, null);
       Counter(1, 3, FeedbackToConsole);
       Console.WriteLine();
    }
-
+   
    private static void Counter(Int32 from, Int32 to, Feedback fb) {
       for (Int32 val = from; val <= to; val++) {
          // If any callbacks are specified, call them
