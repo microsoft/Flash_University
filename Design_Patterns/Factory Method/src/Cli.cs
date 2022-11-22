05298eedb0f61cd3276c61afb280d3e44202c084
Tate Burns <tateburns@microsoft.com>
2022-11-22 11:45:44 -0600

    add shared utils project

--- Design_Patterns/Factory Method/src/Cli.cs
+++ Design_Patterns/Factory Method/src/Cli.cs
@@ -1,4 +1,5 @@
 ﻿using Factory_Method.Analyzer;
+using Utils;
 
 namespace Factory_Method;
 
@@ -9,26 +10,24 @@ namespace Factory_Method;
 /**
  * A CLI for calculating simple statistics on generated number lists
  */
-public class Cli
+public class Cli : CliBase
 {
     public Cli()
     {
         _analyzer = new DatasetAnalyzer();
 
-        _commands = new()
+        Commands.MergeIn(new Dictionary<string, Action>()
         {
             { "switch-dataset", SwitchDataset },
             { "run-analysis", RunAnalysis }
-        };
+        });
     }
 
-    public static string Title = "Dataset Analyzer";
+    public override string Title => "Dataset Analyzer";
 
     private DatasetAnalyzer _analyzer;
 
     #region commands
-
-    private readonly Dictionary<string, Action> _commands;
 
     /**
      * Switches which type of dataset is being used
@@ -53,14 +52,12 @@ public class Cli
         while (num == null)
         {
             Console.Write("> ");
-            var input = Console.ReadLine();
 
-
-            if (int.TryParse(input, out var value))
+            try
             {
-                num = value;
+                num = CliUtils.ReadInt();
             }
-            else
+            catch
             {
                 Console.WriteLine("not a valid number");
             }
@@ -82,51 +79,6 @@ public class Cli
         Console.WriteLine($"average: {summary.Average}");
         Console.WriteLine($"median: {summary.Median}");
         Console.WriteLine($"sum: {summary.Sum}");
-    }
-
-    #endregion
-
-    #region command runner
-
-    public void Run()
-    {
-        Console.WriteLine(Title);
-
-        var command = GetNextCommand();
-
-        while (command != null && command != "quit" && command != "exit")
-        {
-            RunCommand(command);
-            command = GetNextCommand();
-        }
-    }
-
-    private string? GetNextCommand()
-    {
-        Console.Write("\n> ");
-        return Console.ReadLine();
-    }
-
-    private void RunCommand(string command)
-    {
-        try
-        {
-            if (_commands.TryGetValue(command, out var commandAction))
-            {
-                commandAction();
-            }
-            else
-            {
-                String.Join("', '", _commands.Keys);
-
-                Console.WriteLine("unknown command: {0}", command);
-                Console.WriteLine($"valid commands are: '{String.Join("', '", _commands.Keys)}'");
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error: {0}", e.Message);
-        }
     }
 
     #endregion
---
+++ Design_Patterns/Utils/Cli.cs
@@ -0,0 +1,131 @@
+﻿namespace Utils;
+
+public abstract class CliBase
+{
+    public CliBase()
+    {
+        Commands = new()
+        {
+            { "help", Help }
+        };
+    }
+
+    public CliBase(Dictionary<string, Action> commands)
+    {
+        Commands = commands;
+        Commands.Add("help", Help);
+    }
+
+    public abstract string Title { get; }
+
+    protected Dictionary<string, Action> Commands { get; }
+
+    #region command runner
+
+    public void Run()
+    {
+        Console.WriteLine(Title);
+
+        var command = GetNextCommand();
+
+        while (command != null && command != "quit" && command != "exit")
+        {
+            RunCommand(command);
+            command = GetNextCommand();
+        }
+    }
+
+    private string? GetNextCommand()
+    {
+        Console.Write("\n> ");
+        return Console.ReadLine();
+    }
+
+    private void RunCommand(string command)
+    {
+        try
+        {
+            if (Commands.TryGetValue(command, out var commandAction))
+            {
+                commandAction();
+            }
+            else
+            {
+                String.Join("', '", Commands.Keys);
+
+                Console.WriteLine("unknown command: {0}", command);
+                Help();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e.Message);
+        }
+    }
+
+    private void Help()
+    {
+        Console.WriteLine($"valid commands are: '{String.Join("', '", Commands.Keys)}', 'quit', 'exit'");
+    }
+
+    #endregion
+}
+
+public static class CliUtils
+{
+    #region Methods for reading input
+
+    public static int ReadInt()
+    {
+        var rawValue = Console.ReadLine();
+
+        if (int.TryParse(rawValue, out var value))
+        {
+            return value;
+        }
+        else
+        {
+            throw new Exception($"{rawValue} is not a valid int");
+        }
+    }
+
+    public static string ReadMultipleLines()
+    {
+        var text = "";
+        var line = Console.ReadLine() ?? "";
+
+        while (line != "")
+        {
+            if (text != "") text += "\n";
+            text += line;
+
+            line = Console.ReadLine() ?? "";
+        }
+
+        return text;
+    }
+
+    public static bool ReadYesNo()
+    {
+        var rawValue = Console.ReadLine() ?? "";
+        return rawValue.Equals("y", StringComparison.OrdinalIgnoreCase);
+    }
+
+    #endregion
+}
+
+public static class CommandExtensions
+{
+    public static void MergeIn<TKey, TValue>(this IDictionary<TKey, TValue> target, IDictionary<TKey, TValue> source)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        if (target == null)
+            throw new ArgumentNullException(nameof(target));
+
+        foreach (var keyValue in source)
+        {
+            target.Add(keyValue.Key, keyValue.Value);
+        }
+    }
+}
