a46ff492de6c57ee7541f05f006bdecbd6312a99
rakirs2 <srmylavarapu@microsoft.com>
2023-01-04 14:38:55 -1000

    runtime serialization

---
+++ CLR_via_CSharp/24_RuntimeSerialization/RuntimeSerialization.md
@@ -0,0 +1,16 @@
+# Runtime Serialization
+
+## Goals
+
+## Why is Serialization useful
+- at the end of the day, everything gets stored in bytes
+- so the ability to convert something to and fro bytes is at the core of what computers do
+- Say we copy an item and use it in a different app, how does that work?
+
+## Glossary
+
+### Serialization
+process of converting an object or graph of connected objects into a stream of bytes.
+
+### Deserialization
+converting a stream of bytes into an object/graph of connected objects
---
+++ CLR_via_CSharp/24_RuntimeSerialization/RuntimeSerialization/Introduction.cs
@@ -0,0 +1,45 @@
+﻿// INitial example from the book
+
+using System.Diagnostics;
+using System.Runtime.Serialization.Formatters.Binary;
+
+var objectGraph = new List<String>{"one", "two", "three"};
+Stream stream = SerializeToMemory(objectGraph);
+
+// Reset from 0
+
+stream.Position = 0;
+objectGraph = null;
+
+objectGraph = (List<String>)DeserializeFromMemory(stream);
+foreach (var s in objectGraph)
+{
+    Console.WriteLine(s);
+}
+
+static MemoryStream SerializeToMemory(Object objectGraph)
+{
+    // create a stream w/ the objects
+    MemoryStream stream = new MemoryStream();
+
+    // Construct a serializer
+    BinaryFormatter formatter = new BinaryFormatter();
+    BinaryReader reader = new BinaryReader(stream);
+    //reader.ReadBytes(objectGraph);
+    // this is the outdated way of doing it
+#pragma warning disable SYSLIB0011
+    formatter.Serialize(stream, objectGraph);
+#pragma warning restore SYSLIB0011
+
+    return stream;
+}
+
+static Object DeserializeFromMemory(Stream stream)
+{
+
+    // Construct a serializer
+    BinaryFormatter formatter = new BinaryFormatter();
+#pragma warning disable SYSLIB0011
+    return formatter.Deserialize(stream);
+#pragma warning restore SYSLIB0011
+}
