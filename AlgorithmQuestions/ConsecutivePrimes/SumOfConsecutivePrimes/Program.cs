7e2de06ccc902f3e358590de94ca18b2de964cf8
Srikar Mylavarapu <rakirs@Srikars-Mac-mini.local>
2023-02-11 05:19:20 -0600

    working on solution for sliding window

---
+++ AlgorithmQuestions/ConsecutivePrimes/SumOfConsecutivePrimes/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> primesList = new List<int>(){2};
+
+int maxValue = 50;
+
+int i = 2;
+
+
+while (i < maxValue)
+{
+    bool isPrime = true;
+
+    foreach (int prime in primesList)
+    {
+        if (i % prime == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        primesList.Append(i);
+        Console.WriteLine(i);
+    }
+    i++;
+}
+// public static 
+// for (int  =primesList.Count-1;)
+// int left = 0;
+// int right = 0;
+// int sum = 0;
